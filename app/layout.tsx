47f043ecdc5b667ccaa3bb80367f1db705024e50
王哲玮 <m13636472690@163.com>
2026-02-27 18:22:40 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,13 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
